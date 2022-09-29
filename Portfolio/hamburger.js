952365ef80d204786fa54fc06b4b23f263c25179
Udbhav Patel <86018969+Ski1ledHawkeye@users.noreply.github.com>
2022-09-29 22:32:23 +0530

    Delete Portfolio directory

--- Portfolio/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("activek");
-    navMenu.classList.toggle("activek");
-})
